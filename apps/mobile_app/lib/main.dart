1dff124fed084578023ed843146a3cabd0e769ef
fatalmonk <anwar.alvi18@gmail.com>
2026-04-23 04:06:38 +0600

    fix(mobile_app): use non-synthetic localization to fix Vercel build failure

--- apps/mobile_app/lib/main.dart
+++ apps/mobile_app/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'l10n/generated/app_localizations.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
